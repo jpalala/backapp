362f27da4bcfe914d46d889d9041ae8db73ac6bf
Joe Palala <Jose.Palala@live.com>
2021-06-09 18:16:16 +0000

    Added code to the backup command
     - added storage dir
     - updated README
     - added packages

--- app/Commands/DatabaseBackupCommand.php
+++ app/Commands/DatabaseBackupCommand.php
@@ -3,7 +3,10 @@
 namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
 use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class DatabaseBackupCommand extends Command
 {
@@ -12,14 +15,14 @@ class DatabaseBackupCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'command:backup {dbname?} {dbuser?} {dbpass?}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Database backup';
 
     /**
      * Execute the console command.
@@ -28,7 +31,56 @@ class DatabaseBackupCommand extends Command
      */
     public function handle()
     {
-        //
+      $today = today()->format('Y-m-d');
+      if(!is_dir(storage_path('backups'))) {
+        mkdir(storage_path('backups'));
+      }
+      
+      // setup defaults from the config 
+      $default_dbname = config('database.connections.mysql.database');
+      $default_dbuser = config('database.connections.mysql.username');
+      $default_dbpass = config('database.connections.mysql.password');
+
+      // so that options override the config from env. 
+      $dbname = $this->option('dbname') ?? $default_dbname;
+      $dbuser = $this->option('dbuser') ?? $default_dbuser;
+      $dbpass = $this->option('dbpass') ?? $default_dbpass;
+
+      // If anything is empty, it's over
+      if(empty($dbname)) {
+        Log::error('Database name not specified');
+	$this->error('Empty DB name');
+	return;
+      }
+
+      if(empty($dbuser)) {
+        Log::error('Database username not specified');
+	$this->error('Empty DB username');
+	return;
+      }
+
+      if(empty($dbpass)) {
+        Log::error('Database password not specified');
+	$this->error('Empty DB password');
+	return;
+      }
+     
+      // create a process
+      $this->process = new Process(sprintf(
+          'mysqldump --compact --skip-comments -u%s -p%s  %s > %s',   
+	  $dbuser,
+	  $dbpass,
+	  $dbname,
+	  storage_path("backups/{$today}.sql")
+  ));
+
+      // actually try to run the process
+      try {
+        $this->process->mustRun();
+	Log::info('Daily DB backup - is successful!');
+      } catch(ProcessFailedException $exception) {
+        Log.error('Daily DB backup- has failed', $exception);
+      }
     }
 
     /**
@@ -39,6 +91,6 @@ class DatabaseBackupCommand extends Command
      */
     public function schedule(Schedule $schedule): void
     {
-        // $schedule->command(static::class)->everyMinute();
+        $schedule->command(static::class)->dailyAt('08:00');
     }
 }
